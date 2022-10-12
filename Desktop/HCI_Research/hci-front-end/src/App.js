d1d38d7d9efc7a669f8dd2af0b8c0cc7527b7136
achapawa <77375149+achapawa@users.noreply.github.com>
2022-10-12 15:45:59 -0600

    Made Navbar component

--- Desktop/HCI_Research/hci-front-end/src/App.js
+++ Desktop/HCI_Research/hci-front-end/src/App.js
@@ -1,10 +1,11 @@
+import Navbar1 from "./components/Navbar1";
+import Navbar2 from "./components/Navbar2";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>This is our Page :) Yay, congrats Susan and Pawan</h1>
-      </header>
+      <Navbar1/>
+      <Navbar2/>
     </div>
   );
 }
---
+++ Desktop/HCI_Research/hci-front-end/src/components/Navbar2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar2() {
+  return (
+    <header>Header 2</header>
+  )
+}
+
+export default Navbar2
